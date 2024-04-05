1c8bbbdf8269a404d12fbbed07f85532d416c455
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-04-05 21:23:06 +0530

    style: update input

--- src/routes/(grid)/home/+page.svelte
+++ src/routes/(grid)/home/+page.svelte
@@ -1,24 +1,29 @@
-<div class="border-2 border-black h-[calc(100vh-3.5rem)] flex flex-col px-16 py-6 gap-6">
+<script>
+
+	import { Input } from "$lib/components/ui/input";
+
+</script>
+<div class=" h-[calc(100vh-3.5rem)] flex flex-col px-16 py-6 gap-6">
 
     <!-- User input row -->
     <div class="flex justify-between w-full px-8">
 
         <!-- Source -->
-        <div class="flex flex-col ">
-            <h2 class="text-center">Source</h2>
-            <input type="text" name="" id="" class="border border-gray-500">
+        <div class="flex flex-col gap-1">
+            <h2 class="text-center text-xl font-semibold">Source</h2>
+            <Input class="h-8 border-2 shadow-lg" placeholder="Choose Country" />
         </div>
 
         <!-- Desination -->
-        <div class="flex flex-col">
-            <h2 class="text-center">Source</h2>
-            <input type="text" name="" id="" class="border border-gray-500">
+        <div class="flex flex-col gap-1">
+            <h2 class="text-center text-xl font-semibold">Destination</h2>
+            <Input class="h-8 border-2 shadow-lg" placeholder="Hey nigga..." />
         </div>
 
         <!-- Product -->
-        <div class="flex flex-col">
-            <h2 class="text-center">Source</h2>
-            <input type="text" name="" id="" class="border border-gray-500">
+        <div class="flex flex-col gap-1">
+            <h2 class="text-center text-xl font-semibold">Product HS Code</h2>
+            <Input class="h-8 border-2 shadow-lg" placeholder="Hey nigga..." />
         </div>
     </div>
     
