75d98d7c23843e77348eb81743b656af4e930baf
prathwik <64214685+prathwik0@users.noreply.github.com>
2024-04-05 18:57:21 +0530

    assets: icons

---
+++ src/lib/icons/logo-dark.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLAttributes<SVGElement>;
+</script>
+
+<img
+	src="/freix-dark-150x150.png"
+	alt="Freix Logo"
+	height="36"
+	width="36"
+	{...$$restProps}
+/>
---
+++ src/lib/icons/logo.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLAttributes<SVGElement>;
+</script>
+
+<img src="/freix-150x150.png" alt="Freix Logo" height="32" width="32" {...$$restProps} />
