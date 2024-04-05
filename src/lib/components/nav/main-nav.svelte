516ccc767f0af5fbda49de6d8ec7f037fadd26e3
prathwik <64214685+prathwik0@users.noreply.github.com>
2024-04-05 18:56:46 +0530

    feat: navbar components

---
+++ src/lib/components/nav/main-nav.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { siteConfig } from '$lib/config/site';
+	import { cn } from '$lib/utils';
+	import { Icons } from '$lib/icons';
+</script>
+
+<div class="mr-4 hidden md:flex">
+	<a href="/" class="mr-6 flex items-center">
+		<Icons.logoDark class="rounded-full mr-2 hidden dark:block" />
+		<Icons.logo class="rounded-full mr-2 dark:hidden" />
+		<span class="hidden text-[15px] font-bold sm:inline-block lg:text-base">
+			{siteConfig.name}
+		</span>
+	</a>
+	<nav class="flex items-center space-x-6 text-sm font-medium">
+		<a
+			href="/features"
+			class={cn(
+				'transition-colors hover:text-foreground/80',
+				$page.url.pathname === '/features' ? 'text-foreground' : 'text-foreground/60'
+			)}
+		>
+			Features
+		</a>
+		<a
+			href="/docs"
+			class={cn(
+				'transition-colors hover:text-foreground/80',
+				$page.url.pathname.startsWith('/docs') ? 'text-foreground' : 'text-foreground/60'
+			)}
+		>
+			Docs
+		</a>
+		<!-- <a
+			href={siteConfig.links.github}
+			target="_blank"
+			rel="noopener noreferrer"
+			class={cn('hidden text-foreground/60 transition-colors hover:text-foreground/80 xl:block')}
+		>
+			GitHub
+		</a> -->
+	</nav>
+</div>
---
+++ src/lib/components/nav/mobile-nav.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import * as Sheet from '$lib/components/ui/sheet';
+	import { ViewVertical } from 'radix-icons-svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { docsConfig } from '$lib/config/docs';
+	import { siteConfig } from '$lib/config/site';
+	import { Icons } from '$lib/icons';
+	import MobileLink from './mobile-link.svelte';
+
+	let open = false;
+</script>
+
+<Sheet.Root bind:open>
+	<Sheet.Trigger asChild let:builder>
+		<Button
+			builders={[builder]}
+			variant="ghost"
+			class="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+		>
+			<ViewVertical class="h-5 w-5" />
+			<span class="sr-only">Toggle Menu</span>
+		</Button>
+	</Sheet.Trigger>
+	<Sheet.Content side="left" class="pr-0">
+		<MobileLink href="/" class="flex items-center" bind:open>
+			<Icons.logo class="mr-2 h-4 w-4" />
+			<span class="font-bold">{siteConfig.name}</span>
+		</MobileLink>
+		<div class="my-4 h-[calc(100vh-8rem)] overflow-auto pb-10 pl-6">
+			<div class="flex flex-col space-y-3">
+				{#each docsConfig.mainNav as navItem, index (navItem + index.toString())}
+					{#if navItem.href}
+						<MobileLink href={navItem.href} bind:open>
+							{navItem.title}
+						</MobileLink>
+					{/if}
+				{/each}
+			</div>
+			<div class="flex flex-col space-y-2">
+				{#each docsConfig.sidebarNav as navItem, index (index)}
+					<div class="flex flex-col space-y-3 pt-6">
+						<h4 class="font-medium">{navItem.title}</h4>
+						{#if navItem?.items?.length}
+							{#each navItem.items as item}
+								{#if !item.disabled && item.href}
+									<MobileLink href={item.href} bind:open class="text-muted-foreground">
+										{item.title}
+									</MobileLink>
+								{/if}
+							{/each}
+						{/if}
+					</div>
+				{/each}
+			</div>
+		</div>
+	</Sheet.Content>
+</Sheet.Root>
