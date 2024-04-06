9faca7c9336fcda2d85eac60df2bb8e4ebd57548
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2024-04-07 03:48:03 +0530

    style: fix center

--- src/routes/(nonav)/features/ResultTable.svelte
+++ src/routes/(nonav)/features/ResultTable.svelte
@@ -59,59 +59,49 @@
 <Table.Root>
 	<Table.Header>
 		<Table.Row>
-			<Table.Head class="text-center">Type</Table.Head>
-			<Table.Head class="text-center">Percentage</Table.Head>
+			<Table.Head>Type</Table.Head>
+			<Table.Head>Percentage</Table.Head>
 		</Table.Row>
 	</Table.Header>
 	<Table.Body>
 		{#if formData.data}
 			<Table.Row>
-				<Table.Cell class="py-0 text-center font-medium">Import Duty</Table.Cell>
-				<Table.Cell class="py-0 text-center">{formData.data[0].values['import-duty']}</Table.Cell>
+				<Table.Cell class="py-0  font-medium">Import Duty</Table.Cell>
+				<Table.Cell class="py-0 ">{formData.data[0].values['import-duty']}</Table.Cell>
 			</Table.Row>
 			<Table.Row>
-				<Table.Cell class="py-0 text-center font-medium">Value Added Tax</Table.Cell>
-				<Table.Cell class="py-0 text-center"
-					>{formData.data[0].values['value-added-tax']}</Table.Cell
-				>
+				<Table.Cell class="py-0  font-medium">Value Added Tax</Table.Cell>
+				<Table.Cell class="py-0 ">{formData.data[0].values['value-added-tax']}</Table.Cell>
 			</Table.Row>
 			<Table.Row>
-				<Table.Cell class="py-0 text-center font-medium">Custom Clearance Fee</Table.Cell>
-				<Table.Cell class="py-0 text-center"
-					>{formData.data[0].values['custom-clearance-fee']}</Table.Cell
-				>
+				<Table.Cell class="py-0  font-medium">Custom Clearance Fee</Table.Cell>
+				<Table.Cell class="py-0 ">{formData.data[0].values['custom-clearance-fee']}</Table.Cell>
 			</Table.Row>
 			<Table.Row>
-				<Table.Cell class="py-0 text-center font-medium">Exercise Duties</Table.Cell>
-				<Table.Cell class="py-0 text-center"
-					>{formData.data[0].values['exercise-duties']}</Table.Cell
-				>
+				<Table.Cell class="py-0  font-medium">Exercise Duties</Table.Cell>
+				<Table.Cell class="py-0 ">{formData.data[0].values['exercise-duties']}</Table.Cell>
 			</Table.Row>
 			<Table.Row>
-				<Table.Cell class="py-0 text-center font-medium">Tariffs</Table.Cell>
-				<Table.Cell class="py-0 text-center">{formData.data[0].values['tariffs']}</Table.Cell>
+				<Table.Cell class="py-0  font-medium">Tariffs</Table.Cell>
+				<Table.Cell class="py-0 ">{formData.data[0].values['tariffs']}</Table.Cell>
 			</Table.Row>
 			<Table.Row>
-				<Table.Cell class="py-0 text-center font-medium">Anti Dumping Duties</Table.Cell>
-				<Table.Cell class="py-0 text-center"
-					>{formData.data[0].values['anti-dumping-duties']}</Table.Cell
-				>
+				<Table.Cell class="py-0  font-medium">Anti Dumping Duties</Table.Cell>
+				<Table.Cell class="py-0 ">{formData.data[0].values['anti-dumping-duties']}</Table.Cell>
 			</Table.Row>
 			<Table.Row>
-				<Table.Cell class="py-0 text-center font-medium">Environmental Tax</Table.Cell>
-				<Table.Cell class="py-0 text-center"
-					>{formData.data[0].values['environmental-tax']}</Table.Cell
-				>
+				<Table.Cell class="py-0  font-medium">Environmental Tax</Table.Cell>
+				<Table.Cell class="py-0 ">{formData.data[0].values['environmental-tax']}</Table.Cell>
 			</Table.Row>
 			<Table.Row>
-				<Table.Cell class="py-0 text-center font-medium">Documentation</Table.Cell>
-				<Table.Cell class="py-0 text-center">{formData.data[0].values['documentation']}</Table.Cell>
+				<Table.Cell class="py-0  font-medium">Documentation</Table.Cell>
+				<Table.Cell class="py-0 ">{formData.data[0].values['documentation']}</Table.Cell>
 			</Table.Row>
 		{:else}
 			{#each response as item}
 				<Table.Row>
-					<Table.Cell class="py-0 text-center font-medium">{item['Charge']}</Table.Cell>
-					<Table.Cell class="py-0 text-center">{item['Percentage']}</Table.Cell>
+					<Table.Cell class="py-0  font-medium">{item['Charge']}</Table.Cell>
+					<Table.Cell class="py-0 ">{item['Percentage']}</Table.Cell>
 				</Table.Row>
 			{/each}
 		{/if}
