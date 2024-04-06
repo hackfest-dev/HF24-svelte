a765fe62c9aa26d6c876d4cf24a13f521f80602c
prathwik <64214685+prathwik0@users.noreply.github.com>
2024-04-06 15:08:43 +0530

    feat: delete gemini API endpoint and make it a component

--- src/lib/gemini/gemini.ts
+++ src/lib/gemini/gemini.ts
@@ -1,21 +1,26 @@
 import { PUBLIC_GEMINI_API_KEY } from '$env/static/public';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
 
-export const POST: RequestHandler = async ({ request }) => {
-	let { prompt } = await request.json();
+export const geminiFetch = async (url: string): Promise<any> => {
+	let prompt =
+		'What is the total shipping cost and total taxes and duties for a shipment from the US to Canada with a total value of $1000?';
+
 	const genAI = new GoogleGenerativeAI(PUBLIC_GEMINI_API_KEY);
 	const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+
 	prompt =
 		'Please return values only in this format for upcoming queries : {"total_shipping_cost":"cost","total_taxes_and_duties":"cost"}' +
 		prompt;
 
 	const result = await model.generateContent(prompt);
-	const response = await result.response;
+	console.log(result);
+
+	const response = result.response;
 	const text = response.text();
 	console.log(text);
-	return json({
-		data: text
-	});
+
+	// return json({
+	// 	data: text
+	// });
 };
