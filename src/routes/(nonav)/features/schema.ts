1f37fc59a017879a7350b13540bebb614ea46de9
prathwik <64214685+prathwik0@users.noreply.github.com>
2024-04-07 03:37:08 +0530

    Update schema.ts

--- src/routes/(nonav)/features/schema.ts
+++ src/routes/(nonav)/features/schema.ts
@@ -5,18 +5,20 @@ const initialFormValues = {
 	dest: 'Initial Destination',
 	product: 'Initial Product',
 	pathCoordinatesArray: [[{ lat: 0, lng: 0 }]],
-	gem: 'Initial Gem'
+	gem: ''
 };
 
 export const formSchema = z.object({
 	source: z.string().min(1).default(initialFormValues.source),
 	dest: z.string().min(1).default(initialFormValues.dest),
 	product: z.string().min(1).default(initialFormValues.product),
-	pathCoordinatesArray: z.array(z.array(
-		z.object({
-			lat: z.number().default(initialFormValues.pathCoordinatesArray[0][0].lat),
-			lng: z.number().default(initialFormValues.pathCoordinatesArray[0][0].lng)
-		}))
+	pathCoordinatesArray: z.array(
+		z.array(
+			z.object({
+				lat: z.number().default(initialFormValues.pathCoordinatesArray[0][0].lat),
+				lng: z.number().default(initialFormValues.pathCoordinatesArray[0][0].lng)
+			})
+		)
 	),
 	gem: z.string().default(initialFormValues.gem)
 });
