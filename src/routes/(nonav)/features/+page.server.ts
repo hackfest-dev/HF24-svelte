f1567bbb340d3a46f2c68c892e224dfd83c36726
prathwik <64214685+prathwik0@users.noreply.github.com>
2024-04-06 15:07:22 +0530

    feat: refactor home page into components

---
+++ src/routes/(nonav)/features/+page.server.ts
@@ -0,0 +1,32 @@
+import type { PageServerLoad, Actions } from './$types';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { formSchema } from './schema';
+import { error, fail, redirect } from '@sveltejs/kit';
+import { geminiFetch } from '$lib/gemini/gemini';
+
+export const load = (async () => {
+	return {
+		form: await superValidate(zod(formSchema))
+	};
+}) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	default: async (event) => {
+		const form = await superValidate(event, zod(formSchema));
+
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+
+		console.log('form.data', form.data);
+
+		geminiFetch('asdf');
+
+		return {
+			form
+		};
+	}
+};
